f86b4654fe17d7322297a01d1f8c6076ff2bb6c7
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-03-12 22:57:24 +0900

    Add generated files

---
+++ lib/ember/components.rb
@@ -0,0 +1,8 @@
+require "ember/components/version"
+
+module Ember
+  module Components
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ember/components/version.rb
@@ -0,0 +1,5 @@
+module Ember
+  module Components
+    VERSION = "0.1.0"
+  end
+end
