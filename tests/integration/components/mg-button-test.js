654619191256c8a778440007fab2c9f16185f247
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-03 16:19:34 +0900

    mg-button を Octane 用の記述に書き直した

--- tests/integration/components/mg-button-test.js
+++ tests/integration/components/mg-button-test.js
@@ -7,11 +7,44 @@ module('Integration | Component | mg button', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-
-    await render(hbs`{{mg-button text="TEXT"}}`);
+    await render(hbs`<MgButton @text="TEXT" />`);
 
     assert.dom('.btn').hasText('TEXT');
+  });
+
+  test('無効フラグが立てられるとボタンが無効になる', async function(assert) {
+    await render(hbs`<MgButton @disabled={{true}} />`);
+
+    assert.dom('.btn').isDisabled();
+  });
+
+  test('無効フラグが渡されなければボタンは無効にならない', async function(assert) {
+    await render(hbs`<MgButton />`);
+
+    assert.dom('.btn').isNotDisabled();
+  });
+
+  test('プライマリフラグが立てられるとプライマリ用の class が付与される', async function(assert) {
+    await render(hbs`<MgButton @isPrimary={{true}} />`);
+
+    assert.dom('.btn').hasClass('btn-primary');
+  });
+
+  test('プライマリフラグが渡されなければとプライマリ用の class は付与されない', async function(assert) {
+    await render(hbs`<MgButton />`);
+
+    assert.dom('.btn').doesNotHaveClass('btn-primary');
+  });
+
+  test('デンジャーフラグが立てられるとデンジャー用の class が付与される', async function(assert) {
+    await render(hbs`<MgButton @isDanger={{true}} />`);
+
+    assert.dom('.btn').hasClass('btn-danger');
+  });
+
+  test('デンジャーフラグが渡されなければとデンジャー用の class は付与されない', async function(assert) {
+    await render(hbs`<MgButton />`);
+
+    assert.dom('.btn').doesNotHaveClass('btn-danger');
   });
 });
