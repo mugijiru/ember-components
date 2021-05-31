88bf95c3a218d74b61f940ae8cf6e04b046b70d5
mugijiru <106833+mugijiru@users.noreply.github.com>
2021-05-31 23:48:28 +0900

    HTML の class 名変更に伴うテストの修正

--- tests/integration/components/mg-button-test.js
+++ tests/integration/components/mg-button-test.js
@@ -9,43 +9,43 @@ module('Integration | Component | mg button', function (hooks) {
   test('it renders', async function (assert) {
     await render(hbs`<MgButton @text="TEXT" />`)
 
-    assert.dom('.btn').hasText('TEXT')
+    assert.dom('.mg-button').hasText('TEXT')
   })
 
   test('無効フラグが立てられるとボタンが無効になる', async function (assert) {
     await render(hbs`<MgButton @disabled={{true}} />`)
 
-    assert.dom('.btn').isDisabled()
+    assert.dom('.mg-button').isDisabled()
   })
 
   test('無効フラグが渡されなければボタンは無効にならない', async function (assert) {
     await render(hbs`<MgButton />`)
 
-    assert.dom('.btn').isNotDisabled()
+    assert.dom('.mg-button').isNotDisabled()
   })
 
   test('プライマリフラグが立てられるとプライマリ用の class が付与される', async function (assert) {
     await render(hbs`<MgButton @isPrimary={{true}} />`)
 
-    assert.dom('.btn').hasClass('btn-primary')
+    assert.dom('.mg-button').hasClass('mg-button--primary')
   })
 
   test('プライマリフラグが渡されなければとプライマリ用の class は付与されない', async function (assert) {
     await render(hbs`<MgButton />`)
 
-    assert.dom('.btn').doesNotHaveClass('btn-primary')
+    assert.dom('.mg-button').doesNotHaveClass('mg-button--primary')
   })
 
   test('デンジャーフラグが立てられるとデンジャー用の class が付与される', async function (assert) {
     await render(hbs`<MgButton @isDanger={{true}} />`)
 
-    assert.dom('.btn').hasClass('btn-danger')
+    assert.dom('.mg-button').hasClass('mg-button--danger')
   })
 
   test('デンジャーフラグが渡されなければとデンジャー用の class は付与されない', async function (assert) {
     await render(hbs`<MgButton />`)
 
-    assert.dom('.btn').doesNotHaveClass('btn-danger')
+    assert.dom('.mg-button').doesNotHaveClass('mg-button--danger')
   })
 
   test('click すると外部から渡されたアクションが実行される', async function (assert) {
@@ -58,6 +58,6 @@ module('Integration | Component | mg button', function (hooks) {
 
     await render(hbs`<MgButton @onClick={{this.clickAction}} />`)
 
-    click('.btn')
+    click('.mg-button')
   })
 })
--- tests/integration/components/mg-checkbox-test.js
+++ tests/integration/components/mg-checkbox-test.js
@@ -6,24 +6,24 @@ import hbs from 'htmlbars-inline-precompile'
 module('Integration | Component | mg checkbox', function (hooks) {
   setupRenderingTest(hooks)
 
-  test('checked=false の時は class に `c-checkbox--checked` が付与されない', async function (assert) {
+  test('checked=false の時は class に `mg-checkbox--checked` が付与されない', async function (assert) {
     await render(hbs`{{mg-checkbox checked=false}}`)
 
-    assert.dom('.c-checkbox').hasNoClass('c-checkbox--checked')
+    assert.dom('.mg-checkbox').hasNoClass('mg-checkbox--checked')
   })
 
-  test('checked=true の時は class に `c-checkbox--checked` が付与される', async function (assert) {
+  test('checked=true の時は class に `mg-checkbox--checked` が付与される', async function (assert) {
     await render(hbs`{{mg-checkbox checked=true}}`)
-    assert.dom('.c-checkbox').hasClass('c-checkbox--checked')
+    assert.dom('.mg-checkbox').hasClass('mg-checkbox--checked')
   })
 
   test('checkedに渡された値が切り替わると表示も切り替わる', async function (assert) {
     this.set('flag', false)
     await render(hbs`<MgCheckbox @checked={{this.flag}} />`)
-    assert.dom('.c-checkbox').doesNotHaveClass('c-checkbox--checked')
+    assert.dom('.mg-checkbox').doesNotHaveClass('mg-checkbox--checked')
 
     this.set('flag', true)
-    assert.dom('.c-checkbox').hasClass('c-checkbox--checked')
+    assert.dom('.mg-checkbox').hasClass('mg-checkbox--checked')
   })
 
   test('click すると外部から渡されたアクションが実行される', async function (assert) {
@@ -36,6 +36,6 @@ module('Integration | Component | mg checkbox', function (hooks) {
 
     await render(hbs`<MgCheckbox @onClick={{this.clickAction}} />`)
 
-    click('.c-checkbox')
+    click('.mg-checkbox')
   })
 })
